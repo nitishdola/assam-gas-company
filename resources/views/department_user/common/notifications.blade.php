87c85318e72c19616804ae6c2416748050a46153
Nitish Dolakasharia <nitish.dola@gmail.com>
2016-08-30 11:50:25 +0530

    notification in header for department user

---
+++ resources/views/department_user/common/notifications.blade.php
@@ -0,0 +1,87 @@
+<!-- BEGIN NOTIFICATION DROPDOWN -->
+	<li class="dropdown" id="header-notification">
+	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+		<i class="fa fa-bell"></i>
+		<span class="badge">7</span>						
+	</a>
+	<ul class="dropdown-menu notification">
+		<li class="dropdown-title">
+			<span><i class="fa fa-bell"></i> 7 Notifications</span>
+		</li>
+		<li>
+			<a href="#">
+				<span class="label label-success"><i class="fa fa-user"></i></span>
+				<span class="body">
+					<span class="message">5 users online. </span>
+					<span class="time">
+						<i class="fa fa-clock-o"></i>
+						<span>Just now</span>
+					</span>
+				</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<span class="label label-primary"><i class="fa fa-comment"></i></span>
+				<span class="body">
+					<span class="message">Martin commented.</span>
+					<span class="time">
+						<i class="fa fa-clock-o"></i>
+						<span>19 mins</span>
+					</span>
+				</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<span class="label label-warning"><i class="fa fa-lock"></i></span>
+				<span class="body">
+					<span class="message">DW1 server locked.</span>
+					<span class="time">
+						<i class="fa fa-clock-o"></i>
+						<span>32 mins</span>
+					</span>
+				</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<span class="label label-info"><i class="fa fa-twitter"></i></span>
+				<span class="body">
+					<span class="message">Twitter connected.</span>
+					<span class="time">
+						<i class="fa fa-clock-o"></i>
+						<span>55 mins</span>
+					</span>
+				</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<span class="label label-danger"><i class="fa fa-heart"></i></span>
+				<span class="body">
+					<span class="message">Jane liked. </span>
+					<span class="time">
+						<i class="fa fa-clock-o"></i>
+						<span>2 hrs</span>
+					</span>
+				</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<span class="label label-warning"><i class="fa fa-exclamation-triangle"></i></span>
+				<span class="body">
+					<span class="message">Database overload.</span>
+					<span class="time">
+						<i class="fa fa-clock-o"></i>
+						<span>6 hrs</span>
+					</span>
+				</span>
+			</a>
+		</li>
+		<li class="footer">
+			<a href="#">See all notifications <i class="fa fa-arrow-circle-right"></i></a>
+		</li>
+	</ul>
+</li>

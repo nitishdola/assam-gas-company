e9724eeb73cbd9a3cb0db0e04195f98c34fc97f4
Nitish Dolakasharia <nitish.dola@gmail.com>
2016-09-02 16:18:01 +0530

    migration refreshed

--- database/migrations/2016_08_18_111659_create_department_users_table.php
+++ database/migrations/2016_08_18_111659_create_department_users_table.php
@@ -15,6 +15,7 @@ class CreateDepartmentUsersTable extends Migration
         Schema::create('department_users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->integer('designation_id', false, true);
             $table->integer('department_id', false, true);
             $table->integer('section_id', false, true);
             $table->string('username');
@@ -24,6 +25,7 @@ class CreateDepartmentUsersTable extends Migration
             $table->integer('created_by', false, true);
             $table->timestamps();
 
+            $table->foreign('designation_id')->references('id')->on('designations');
             $table->foreign('department_id')->references('id')->on('departments');
             $table->foreign('section_id')->references('id')->on('sections');
             $table->foreign('created_by')->references('id')->on('admins');
